53fa34d4d3a9f78b749125ae03383a2c70101f55
Harsh <hs5686584@gmail.com>
2025-05-21 01:34:42 +0530

    updated code and test push

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, resp) => {
+  resp.send("signup routes");
+};
+
+export const login = (req, resp) => {
+  resp.send("login routes");
+};
+
+export const logout = (req, resp) => {
+  resp.send("logout routes");
+};
