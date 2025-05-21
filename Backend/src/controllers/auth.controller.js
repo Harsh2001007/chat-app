c4916d60a406821ff47d62dab7a0dfd0d2a2a9a0
Harsh <hs5686584@gmail.com>
2025-05-22 01:12:03 +0530

    user model and signin controller part done

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,37 @@
-export const signup = (req, resp) => {
-  resp.send("signup routes");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, resp) => {
+  const { fullname, email, password } = req.body;
+
+  try {
+    if (password.lenght < 6) {
+      return resp
+        .status(400)
+        .json({ message: "password must be more than 6 characters" });
+    }
+
+    const user = User.findOne({ email });
+
+    if (user) {
+      return resp.status(400).json({ message: "user already registered" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //
+    } else {
+      resp.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {}
 };
 
 export const login = (req, resp) => {
