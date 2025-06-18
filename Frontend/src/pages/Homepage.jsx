ac7a77477048d5e362f0c7b91cd5587cfc532bd9
Harsh <hs5686584@gmail.com>
2025-06-19 01:03:50 +0530

    side bar skeleton part done

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useChatStore } from "../store/useChatStore";
+import Sidebar from "../components/Sidebar";
+import ChatContainer from "../components/ChatContainer";
+import NoChatSelected from "../components/NoChatSelected";
 
 const Homepage = () => {
   const { selectedUser } = useChatStore();
